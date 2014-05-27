208bbaa645344b95e286b1d92fbed08a35419c04
Faun <faun@riseup.net>
2014-05-27 15:30:09 +0700

    Replace variable name

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
-source="$( cd `dirname $0` && pwd )/bin/brew_bundle"
+source_script="$( cd `dirname $0` && pwd )/bin/brew_bundle"
 destination_dir="$(brew --prefix)/bin"
 destination="$destination_dir/brew_bundle"
-chmod +x $source
+chmod +x $source_script
 
 if [[ -L $destination ]] || [[ -f $destination ]]
 then
   rm -i $destination
 fi
 
-ln -s $source $destination
+ln -s $source_script $destination
